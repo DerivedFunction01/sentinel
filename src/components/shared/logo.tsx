eb27ea90acf0fe5a3581ffdd5fe45d67cfed4d91
Derived <ytdli08@gmail.com>
2026-06-25 12:33:49 -0400

    change logo

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,3 +1,4 @@
+import { BetweenHorizontalStart } from "lucide-react";
 import React from "react";
 
 interface LogoIconProps {
@@ -27,18 +28,9 @@ export function LogoIcon({
     <div
       className={`flex items-center justify-center bg-slate-950 border border-slate-800/80 shadow-md ${sizeClasses[size]} ${className}`}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="#0da2e7"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={`${iconSizes[size]} ${iconClassName}`}
-      >
-        <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
-      </svg>
+      <BetweenHorizontalStart
+        className={`${iconSizes[size]} ${iconClassName} text-blue-600`}
+      />
     </div>
   );
 }
